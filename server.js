b66b8b8c32dc0afd5c5e7d2ea2c8c74203d01ef4
doodybh <doodybhr38@gmail.com>
2025-12-24 18:13:06 +0300

    learned get requests and responding with a html file

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get(('/'), (req, res) => res.sendFile(__dirname + '/views/index.html') );
+
+app.get('/about', (req, res) => res.sendFile(__dirname + '/views/about.html'))
+
+app.listen(port, () => console.log('The server is running. http://127.0.0.1:3000'))
