e9aa6c19a360025c63e9f56378a34ccff7395c92
doodybh <doodybhr38@gmail.com>
2025-12-26 02:03:17 +0300

    forgot about the /test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-app.get(('/'), (req, res) => res.sendFile(__dirname + '/views/index.html') );
+app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html') );
 
 app.get('/about', (req, res) => res.sendFile(__dirname + '/views/about.html'))
 
