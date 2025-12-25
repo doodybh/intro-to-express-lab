769ee002066638a3eb009fe58e2a7066b91ee613
doodybh <doodybhr38@gmail.com>
2025-12-26 02:02:52 +0300

    forgot about the /test

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.get(('/'), (req, res) => res.sendFile(__dirname + '/views/index.html') );
 
 app.get('/about', (req, res) => res.sendFile(__dirname + '/views/about.html'))
 
+app.get('/test', (req, res) => res.send('Success!'))
+
 app.listen(port, () => console.log('The server is running. http://127.0.0.1:3000'))
